f7879687397bcda24aca746542bdd2c4e4674450
Samar <yakhyaevdev@gmail.com>
2022-10-03 22:40:16 +0900

    Clean code principle

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 let score = 20;
-// let highscore = 0;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -24,20 +24,10 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".highscore").textContent = highscore;
     }
   } // when guess is high
-  else if (guess > secretNumber) {
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "Too high !!!";
-
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "You lost the game !⛔️";
-      document.querySelector(".score").textContent = 0;
-    }
-  } // when guess is low
-  else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Too low !!!";
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? " Too high !!!" : "Too low !!!";
 
       score--;
       document.querySelector(".score").textContent = score;
