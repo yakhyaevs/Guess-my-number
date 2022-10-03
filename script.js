07d50a8b83a15bae5e7e5fabca4b5bd44b4f1a75
Samar <yakhyaevdev@gmail.com>
2022-10-03 16:37:41 +0900

    example

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 "use strict";
-//Guess my number is begin
-//Refrefing all javascript features
 
-document.querySelector(".message").textContent;
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = " No number !!!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number 🎉 !";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too high !!!";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Too low !!!";
+  }
+});
