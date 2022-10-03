7f501754b8df380bc3ce886a4409228de0f97605
Samar <yakhyaevdev@gmail.com>
2022-10-03 20:49:14 +0900

    highScore

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 let score = 20;
+// let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -13,9 +14,15 @@ document.querySelector(".check").addEventListener("click", function () {
   } // when player wins
   else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number 🎉 !";
+    document.querySelector(".highscore").textContent = score;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   } // when guess is high
   else if (guess > secretNumber) {
     if (score > 1) {
